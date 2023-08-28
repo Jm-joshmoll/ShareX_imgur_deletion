4e1ed2c63b72905ee5fbb3b0664287b5b26ff7cd
Josh Waters <Jm.joshmoll@gmail.com>
2023-08-28 11:59:05 +0100

    Update script.py

--- script.py
+++ script.py
@@ -1,9 +1,9 @@
 """
 Script to Open and Delete Files Using Selenium
 
-This script reads entries from a JSON file named 'History.json', extracts the 'DeletionURL'
-from each entry, opens the URL using Selenium WebDriver, and clicks the 'Yes' button to
-confirm the deletion.
+This script reads entries from the shareX JSON file named 'History.json', transforms the content 
+into valid JSON format, extracts the 'DeletionURL' from each entry, opens the URL using Selenium
+WebDriver, and clicks the 'Yes' button to confirm the deletion.
 
 Make sure to install the required dependencies before running the script:
 - Selenium: pip install selenium
@@ -17,32 +17,32 @@ Usage:
 2. Run the script using: python script.py
 """
 
-import re
-import json
+import ast
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
 
 # Path to ShareX's History.json file
-history_file_path = "../Documents/ShareX/History.json"  # Replace with the correct path
+history_file_path = "path/to/History.json"  # Replace with the correct path
 count = 0
-
-# Read the entire file content
-with open(history_file_path, "r", encoding="utf-8") as json_file:
-    json_content = json_file.read()
-
-# Extract individual JSON objects using regular expression
-pattern = re.compile(r'{[^{}]*"DeletionURL"[^{}]*}')
-matches = pattern.findall(json_content)
 
 # Initialize the Selenium web driver
 driver = webdriver.Chrome()  # Make sure to have Chrome driver installed
 
-# Process each extracted JSON object
-for match in matches:
+# Read and preprocess each line from the file
+preprocessed_lines = []
+
+with open(history_file_path, "r", encoding="utf-8") as json_file:
+    for line in json_file:
+        line = line.strip()
+        if line.startswith('"DeletionURL"'):
+            preprocessed_lines.append(line)
+
+# Process each entry in the preprocessed lines
+for line in preprocessed_lines:
     try:
-        entry = json.loads(match)
+        entry = ast.literal_eval("{" + line + "}")
         deletion_url = entry.get("DeletionURL")
 
         if deletion_url:
@@ -61,26 +61,18 @@ for match in matches:
                 print("Timeout while waiting for the 'Yes' button to appear.")
 
             # Wait for a short time before opening the next link
-            time.sleep(2)
+            time.sleep(5)
 
-            # Unsure on imgur's limit
-            if count == 100:
+            # Limit the deletion process to not get rate limited
+            if count == 500:
                 break
 
-            # Remove the processed entry from the JSON content
-            json_content = json_content.replace(match, "")
-            print(f"Deleted: {deletion_url}")
+            count = count + 1
 
-        count = count + 1
-
-    except json.JSONDecodeError:
-        print("Failed to parse JSON:", match)  # Ignore lines that aren't valid JSON objects
-else:
-    print("No entries with DeletionURL found.")
+    except (ValueError, SyntaxError):
+        print("Failed to parse line:", line)  # Ignore lines that aren't valid literal objects
 
 # Close the web driver
 driver.quit()
 
-# Write the modified JSON content back to the file
-with open(history_file_path, "w", encoding="utf-8") as json_file:
-    json_file.write(json_content)
+print("Deletion process completed.")
